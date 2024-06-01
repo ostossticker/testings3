9ec1b31da76408492ec735e56fdce40e56409767
ostossticker <138345708+ostossticker@users.noreply.github.com>
2024-06-01 16:10:44 +0700

    Update page.js

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -3,13 +3,13 @@ import { UploadForm } from "@/app/upload/(form)/form";
 
 export default function Page() {
    const getFormattedDate = (offset) => {
-      const today = new Date();
-      const targetDate = new Date(today);
-      targetDate.setDate(today.getDate() + offset);
-      const day = targetDate.getDate();
-      const month = ('0' + (targetDate.getMonth() + 1)).slice(-2); // Months are zero-indexed, adding leading zero if necessary
-      const year = targetDate.getFullYear();
-      return `${year}-${month}-${day}`; // Format: "2024-05-16"
+       const today = new Date();
+       const targetDate = new Date(today);
+       targetDate.setDate(today.getDate() + offset);
+       const day = ('0' + targetDate.getDate()).slice(-2); // Adding leading zero if necessary
+       const month = ('0' + (targetDate.getMonth() + 1)).slice(-2); // Adding leading zero if necessary
+       const year = targetDate.getFullYear();
+       return `${year}-${month}-${day}`; 
   };
   return(
     <>
