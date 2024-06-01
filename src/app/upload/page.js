7eee608b2f07732531941fdbf10cebd0b5539bf4
ostossticker <138345708+ostossticker@users.noreply.github.com>
2024-06-01 15:58:34 +0700

    Update page.js

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -2,10 +2,21 @@
 import { UploadForm } from "@/app/upload/(form)/form";
 
 export default function Page() {
+   const getFormattedDate = (offset) => {
+      const today = new Date();
+      const targetDate = new Date(today);
+      targetDate.setDate(today.getDate() + offset);
+      const day = targetDate.getDate();
+      const month = ('0' + (targetDate.getMonth() + 1)).slice(-2); // Months are zero-indexed, adding leading zero if necessary
+      const year = targetDate.getFullYear();
+      return `${year}-${month}-${day}`; // Format: "2024-05-16"
+  };
   return(
     <>
       <h1>Upload Files to S3 Bucket</h1>
       <UploadForm />
+      {`${getFormattedDate(-1)}`}
+      {`${getFormattedDate(0)}`}
     </>
   )
 }
